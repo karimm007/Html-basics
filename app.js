793bbc97760bf9f58829353413022fe74c22d1e3
karimm007 <119793757+karimm007@users.noreply.github.com>
2022-12-04 13:45:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+console.log("hello world")
+var nomvariable=100
+console.log(typeof(nomvariable))
+var str="hello"
+console.log(typeof(str))
+var myarray =[1,"hello",3.14]
+console.log(myarray) 
+var x=5
+var y=10
+var somme = x+y  
+var sous = x-y
+var mult =x*y
+var div = x/y 
+console.log(somme,sous,mult,div)
+console.log(Math.sqrt(x))
+console.log(Math.pow(x,2))
+var numberrandom=Math.floor(Math.random()*20+1) 
+console.log(numberrandom) 
+var tableau =[1,2,3,4]
+for (var i=0; i< tableau.length; i++)
+console.log(tableau[i]) 
+var tab=[12,14,52,17,19,23] 
+// for(let j=0 ;j<tab.length;j++)
+// {
+// var existance=tab.includes == 52?"existe":"n'existe pas "
+// }
+
+
+
+
+
+
